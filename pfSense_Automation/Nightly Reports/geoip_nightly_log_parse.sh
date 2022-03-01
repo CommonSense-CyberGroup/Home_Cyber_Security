4868b864107b4a6511d79997c9a5e79880d565c7
CommonSense-CyberGroup <92452576+CommonSense-CyberGroup@users.noreply.github.com>
2022-02-28 21:58:02 -0700

    Update geoip_nightly_log_parse.sh

--- pfSense_Automation/Nightly Reports/geoip_nightly_log_parse.sh
+++ pfSense_Automation/Nightly Reports/geoip_nightly_log_parse.sh
@@ -7,7 +7,7 @@
 #Version: 1.0.1
 #License: MIT
 #Created: 2/26/2022
-#Updated: 2/26/2022
+#Updated: 2/28/2022
 #
 #Purpose:
 #This script is intended to pull out the pfBolckerNG logs (by FW rule ID) from the FW log file. This will pull out the previous days
@@ -20,7 +20,7 @@ curl https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country
 
 
 #Create log file from yesterdays logs
-ydate=$(date -v-1d +"%b %d") && cat /var/log/filter.log | grep "$ydate" | grep "1770008959" > nightly_geoip_log.txt
+ydate=$(date -v-1d +"%b %d") && cat /var/log/filter.log | grep "$ydate" | grep -E "1770008959|1770009276" > nightly_geoip_log.txt
 
 #Call the geoip_nightly_report.py script to create the report and email out
-python3 geoip_nightly_report.py
+python ./geoip_nightly_report.py
